c3760f22567c34f56e45c68e22ad63eb006766c3
DELL <DELL@DESKTOP-SBESDHB.lan>
2025-02-12 11:50:24 +0530

    1

---
+++ src/day1/FirstJavaProgram.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class FirstJavaProgram 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to java");
+		System.out.println("Madhava");
+		System.out.println(10+20);
+	}
+	
+	
+	
+}
---
+++ src/day1/SecondJavaProgram.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class SecondJavaProgram 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Welcome to jav");
+	System.out.println("Second Java program");
+
+	}
+
+}
