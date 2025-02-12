92fdec565e63ac00fe08d16ab53445656f9f3675
DELL <DELL@DESKTOP-SBESDHB.lan>
2025-02-12 16:58:21 +0530

    Day2

---
+++ src/day2/DataTypeDemo.java
@@ -0,0 +1,53 @@
+package day2;
+
+public class DataTypeDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		//Numeric data type
+		int a=100, b=200;
+		System.out.println("the value of a is:"+a);
+		System.out.println("the value of b is:"+b);
+		System.out.println("the sum of a and b is:"+(a+b));
+		
+		byte by=125;
+		System.out.println(by);
+		
+		short sh=3535;
+		System.out.println(sh);
+		
+		long l=123456723456789L;
+		System.out.println(l);
+		
+		
+		//Decimal numbers- float, double
+		
+		float item_price=15.5F;
+		System.out.println(item_price);
+		
+		double dbl=1234.13573365;
+		System.out.println(dbl);
+		
+		char grad='A';
+		System.out.println(grad);
+		
+		String name="Madhava";
+		System.out.println(name);
+		
+		//char ch='ABC';  //invalid
+		//String ch='ABC'; //invalid
+		//String ch='A'; //invalid
+		//String ch="A"; //valid
+		
+//		boolean bl=false; //allows only true or false
+//		System.out.println(bl);
+		
+		//boolean bl="true"; //not valid
+		//boolean bl="false"; //not valid
+		
+		//String bl=true; //not valid
+		String bl="true"; //valid
+	}
+
+}
---
+++ src/day2/VariableDemo.java
@@ -0,0 +1,44 @@
+package day2;
+
+public class VariableDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		//int a; //declaration
+		//a=100;  //Assignment
+		
+//		int a=100; //Declaration and assignment
+//		
+//		System.out.println(a);
+//		
+//		a=200;
+//		
+//		System.out.println(a);
+		
+		
+		//Approch 1 -if all the variables are belongs to different data types
+		
+		/*int a=100;
+		int b=200;
+		int c=300;*/
+		
+		//Approch 2 -if all the variables are belongs to same data type
+		
+		/*int a,b,c;
+		
+		a=100;
+		b=200;
+		c=300;*/
+		
+		//Approch 3 -if all the variables are belongs to same data type
+		int a=100,b=300, c=400;
+		
+		System.out.println("The value of a:" + a);
+		System.out.println("The value of b:" + b);
+		System.out.println("The value of c:" + c);
+		System.out.println(a+" "+b+" "+c);
+		
+	}
+
+}
